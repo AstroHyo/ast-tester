33c4a4bc60a6312ac9a4bb51af989213b95fc7fc
Ethan (Seung Hyo) Lee <52388644+AstroHyo@users.noreply.github.com>
2025-03-27 17:23:34 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,98 +1,231 @@
-//The below is from a github tutorial
-//https://docs.github.com/en/apps/creating-github-apps/writing-code-for-a-github-app/building-a-github-app-that-responds-to-webhook-events
-// These are the dependencies for this file.
-//
-// You installed the `dotenv` and `octokit` modules earlier. The `@octokit/webhooks` 
-// is a dependency of the `octokit` module, so you don't need to install it separately. The `fs` and `http` dependencies are built-in Node.js modules.
-import dotenv from "dotenv";
-import {App} from "octokit";
-import {createNodeMiddleware} from "@octokit/webhooks";
-import fs from "fs";
-import http from "http";
+import "../style.css";
 
-// This reads your `.env` file and adds the variables from that file to the `process.env` object in Node.js.
-dotenv.config();
+import NotesWall from "./notesWall";
 
-// This assigns the values of your environment variables to local variables.
-const appId = process.env.APP_ID;
-const webhookSecret = process.env.WEBHOOK_SECRET;
-const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+class StickyNotesApp {
+  #notesWall;
+  #notesWallElement;
+  #inputNewNote;
 
-// This reads the contents of your private key file.
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+  constructor() {
+    this.#notesWall = new NotesWall();
+    this.#notesWallElement = document.getElementById("notes-wall");
+    this.#inputNewNote = document.getElementById("new-note");
 
-// This creates a new instance of the Octokit App class.
-const app = new App({
-  appId: appId,
-  privateKey: privateKey,
-  webhooks: {
-    secret: webhookSecret
-  },
-});
+    this.#inputNewNote.addEventListener(
+      "keydown",
+      this.#handleKeyDownToCreateNewNote,
+    );
 
-// This defines the message that your app will post to pull requests.
-const messageForNewPRs = `Thanks for opening a new PR! 
-The backend says hello world!
-TGIF!`;
+    this.#notesWallElement.addEventListener(
+      "dblclick",
+      this.#handleDoubleClickOnNoteElement.bind(this),
+    );
 
-// This adds an event handler that your code will call later. When this event handler is called, it will log the event to the console. Then, it will use GitHub's REST API to add a comment to the pull request that triggered the event.
-async function handlePullRequestOpened({octokit, payload}) {
-  console.log(`Received a pull request event for #${payload.pull_request.number}`);
+    this.#notesWallElement.addEventListener(
+      "keydown",
+      this.#handleKeyDownToSaveNote.bind(this),
+    );
 
-  try {
-    await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      issue_number: payload.pull_request.number,
-      body: messageForNewPRs,
-      headers: {
-        "x-github-api-version": "2022-11-28",
-      },
-    });
-  } catch (error) {
-    if (error.response) {
-      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+    this.#notesWallElement.addEventListener(
+      "blur",
+      this.#handleBlurToSaveNote.bind(this),
+      true,
+    );
+
+    this.#notesWallElement.addEventListener(
+      "click",
+      this.#handleClickOnRemoveNoteButton.bind(this),
+    );
+
+    document.addEventListener("DOMContentLoaded", this.#renderNotes.bind(this));
+  }
+
+  // Helper Function to create note text element
+  #createNoteText(note) {
+    const noteText = document.createElement("div");
+    noteText.id = `note-text-${note.id}`;
+    noteText.classList.add("p-4", "note-text");
+    noteText.innerText = note.text;
+    return noteText;
+  }
+
+  // Helper Function to create note edit textarea element
+  #createNoteTextarea(note) {
+    const noteEdit = document.createElement("textarea");
+    noteEdit.id = `note-textarea-${note.id}`;
+    noteEdit.classList.add(
+      "absolute",
+      "top-0",
+      "left-0",
+      "hidden",
+      "w-full",
+      "h-full",
+      "p-4",
+      "transition-transform",
+      "transform",
+      "bg-yellow-300",
+      "shadow-xl",
+      "resize-none",
+      "outline-rose-700",
+      "outline-offset-0",
+      "note-edit",
+      "note",
+      "hover:scale-105",
+    );
+    noteEdit.value = note.text;
+    return noteEdit;
+  }
+
+  // Helper Function to create a remove button
+  #createNoteRemoveButton(note) {
+    const noteRemoveButton = document.createElement("button");
+    noteRemoveButton.id = `note-delete-btn-${note.id}`;
+    noteRemoveButton.classList.add(
+      "absolute",
+      "w-5",
+      "h-5",
+      "leading-5",
+      "text-center",
+      "transition-opacity",
+      "opacity-0",
+      "cursor-pointer",
+      "delete-btn",
+      "top-1",
+      "right-1",
+      "hover:opacity-100",
+    );
+    noteRemoveButton.innerText = "🗑";
+    return noteRemoveButton;
+  }
+
+  // Helper Function to create a note item
+  #createNoteItem(note) {
+    const noteItem = document.createElement("div");
+    noteItem.id = `note-item-${note.id}`;
+    noteItem.classList.add(
+      "relative",
+      "w-40",
+      "h-40",
+      "p-0",
+      "m-2",
+      "overflow-y-auto",
+      "transition-transform",
+      "transform",
+      "bg-yellow-200",
+      "shadow-lg",
+      "note",
+      "hover:scale-105",
+    );
+    noteItem.style.overflow = "hidden";
+    const noteRemoveButton = this.#createNoteRemoveButton(note);
+    const noteText = this.#createNoteText(note);
+    const noteEdit = this.#createNoteTextarea(note);
+    noteItem.append(noteRemoveButton, noteText, noteEdit);
+    return noteItem;
+  }
+
+  // Function to render the notes
+  #renderNotes() {
+    this.#notesWallElement.innerHTML = "";
+    const notesElements = this.#notesWall
+      .getNotes()
+      .map(this.#createNoteItem.bind(this));
+    this.#notesWallElement.append(...notesElements);
+  }
+
+  // Event handler to create a new note item
+  #handleKeyDownToCreateNewNote = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      const noteText = event.target.value.trim();
+      if (noteText) {
+        this.#notesWall.addNote(noteText);
+        event.target.value = ""; // Clear the input
+        this.#renderNotes();
+      }
+    } else if (event.key === "Enter" && event.shiftKey) {
+      event.preventDefault();
+      const noteText = event.target;
+      const cursorPosition = noteText.selectionStart;
+      noteText.value =
+        noteText.value.substring(0, cursorPosition) +
+        "\n" +
+        noteText.value.substring(cursorPosition);
+      noteText.selectionStart = noteText.selectionEnd = cursorPosition + 1;
     }
-    console.error(error)
-  }
-};
+  };
 
-// This sets up a webhook event listener. When your app receives a webhook event from GitHub
-// with a `X-GitHub-Event` header value of `pull_request` and an `action` payload value of `opened`, 
-// it calls the `handlePullRequestOpened` event handler that is defined above.
-app.webhooks.on("pull_request.opened", handlePullRequestOpened);
+  // Event handler to edit a note item
+  #handleDoubleClickOnNoteElement = (event) => {
+    const noteItem = event.target.closest(".note");
+    if (noteItem) {
+      document.querySelectorAll(".note-edit").forEach((noteEdit) => {
+        noteEdit.style.display = "none";
+        noteEdit.previousElementSibling.style.display = "block";
+      });
 
-// This logs any errors that occur.
-app.webhooks.onError((error) => {
-  if (error.name === "AggregateError") {
-    console.error(`Error processing request: ${error.event}`);
-  } else {
-    console.error(error);
-  }
-});
+      const noteText = noteItem.querySelector(".note-text");
+      const noteEdit = noteItem.querySelector(".note-edit");
 
-// This determines where your server will listen.
-//
-// For local development, your server will listen to port 3000 on `localhost`. When you deploy 
-// your app, you will change these values. For more information, see [Deploy your app](#deploy-your-app).
-const port = 3000;
-const host = 'localhost';
-const path = "/api/webhook";
-const localWebhookUrl = `http://${host}:${port}${path}`;
+      noteText.style.display = "none";
+      noteEdit.style.display = "block";
+      noteEdit.classList.add("scale-105");
+      noteEdit.focus();
+    }
+  };
 
-// This sets up a middleware function to handle incoming webhook events.
-//
-// Octokit's `createNodeMiddleware` function takes care of generating this middleware function for you. The resulting middleware function will:
-//
-//    - Check the signature of the incoming webhook event to make sure that it matches your webhook secret. This verifies that the incoming webhook event is a valid GitHub event.
-//    - Parse the webhook event payload and identify the type of event.
-//    - Trigger the corresponding webhook event handler.
-const middleware = createNodeMiddleware(app.webhooks, {path});
+  // Event handler to save the note when clicking outside or pressing Enter/Escape
+  #handleBlurToSaveNote = (event) => {
+    if (event.target.classList.contains("note-edit")) {
+      const noteText = event.target.value.trim();
+      const noteId = this.#findTargetNoteElement(event.target);
+      const noteIdNumber = this.#parseNoteId(noteId);
+      if (noteText) {
+        this.#notesWall.editNote(noteIdNumber, noteText);
+        this.#renderNotes();
+      }
+    }
+  };
 
-// This creates a Node.js server that listens for incoming HTTP requests (including webhook payloads from GitHub) on the specified port. When the server receives a request, it executes the `middleware` function that you defined earlier. Once the server is running, it logs messages to the console to indicate that it is listening.
-http.createServer(middleware).listen(port, () => {
-  console.log(`Server is listening for events at: ${localWebhookUrl}`);
-  console.log('Press Ctrl + C to quit.')
-});
+  // Event handler to save the edited target note element
+  #handleKeyDownToSaveNote = (event) => {
+    if (event.target.classList.contains("note-edit")) {
+      const noteId = this.#parseNoteId(event.target);
+      if (
+        (event.key === "Enter" || event.key === "Escape") &&
+        !event.shiftKey
+      ) {
+        event.preventDefault();
+        this.#handleBlurToSaveNote(event);
+      } else if (event.key === "Enter" && event.shiftKey) {
+        event.preventDefault();
+        const noteText = event.target;
+        const cursorPosition = noteText.selectionStart;
+        noteText.value =
+          noteText.value.substring(0, cursorPosition) +
+          "\n" +
+          noteText.value.substring(cursorPosition);
+        noteText.selectionStart = noteText.selectionEnd = cursorPosition + 1;
+      }
+    }
+  };
 
-///
+  // Helper function to find the target todo element
+  #findTargetNoteElement = (element) => element.closest(".note");
+
+  // Helper function to parse the note id from the note element
+  #parseNoteId = (note) => (note ? Number(note.id.split("-").pop()) : -1);
+
+  // Event handler to remove the note
+  #handleClickOnRemoveNoteButton = (event) => {
+    if (event.target.id.startsWith("note-delete-btn-")) {
+      const noteId = this.#findTargetNoteElement(event.target);
+      const noteIdNumber = this.#parseNoteId(noteId);
+      this.#notesWall.removeNote(noteIdNumber);
+      this.#renderNotes();
+    }
+  };
+}
+
+new StickyNotesApp();
