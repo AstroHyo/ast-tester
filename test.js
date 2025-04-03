d44b311fb4d05f2a460e2cfafae27edaa93312f0
Ethan (Seung Hyo) Lee <52388644+AstroHyo@users.noreply.github.com>
2025-04-03 16:59:50 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,36 @@ function add1(a, b) {
     return a - b;
   }
 }
+
+function add1(a, b) {
+  if (a >= b) {
+    return a + b;
+  } else {
+    return a - b;
+  }
+}
+
+function add1(a, b) {
+  if (a >= b) {
+    return a + b;
+  } else {
+    return a - b;
+  }
+}
+
+function add1(a, b) {
+  if (a >= b) {
+    return a + b;
+  } else {
+    return a - b;
+  }
+}
+
+function add1(a, b) {
+  if (a >= b) {
+    return a + b;
+  } else {
+    return a - b;
+  }
+}
+
