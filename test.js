5d7c4b4a718e35605a0fb752c87faf4904876727
Ethan (Seung Hyo) Lee <52388644+AstroHyo@users.noreply.github.com>
2025-04-04 00:29:49 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,80 +1,88 @@
-// Test.js - 의도적으로 문제를 포함한 테스트 파일
+// test.js
 
-"use strict";
-
-// 중복 함수 선언 (strict mode에서는 에러 발생)
-function add1(a, b) {
-  return a + b;
-}
-
-function add1(a, b) {
-  return a - b;
-}
-
-// 깊은 중첩과 높은 순환 복잡도를 가진 함수
-function complexFunction(x) {
-  if (x > 0) {
-    for (let i = 0; i < x; i++) {
-      if (i % 2 === 0) {
-        while (x > 0) {
-          x--;
-          if (x === 5) {
-            switch (x) {
-              case 5:
-                console.log("x is 5");
-                break;
-              default:
-                console.log("x is not 5");
-            }
-          }
-        }
+// 복잡하고 중첩이 깊은 함수 예제
+function complexFunction(a, b, c) {
+  let result = 0;
+  if (a > 0) {
+    if (b > 0) {
+      if (c > 0) {
+        result += 1;
+      } else if (c === 0) {
+        result += 2;
       } else {
-        do {
-          x--;
-        } while (x > 0);
+        result += 3;
+      }
+    } else if (b === 0) {
+      for (let i = 0; i < 5; i++) {
+        if (i % 2 === 0) {
+          result += i;
+        } else {
+          result -= i;
+        }
+      }
+    } else {
+      switch(b) {
+        case -1:
+          result += 10;
+          break;
+        case -2:
+          result += 20;
+          break;
+        default:
+          result += 5;
       }
     }
   } else {
-    try {
-      // 의도적인 에러: 정의되지 않은 함수를 호출함.
-      undefinedFunctionCall();
-    } catch (e) {
-      console.error("Caught error:", e);
-    }
-  }
-}
-
-// 메서드가 과도하게 많은 클래스를 통한 코드 스멜 예시
-class OverloadedClass {
-  method1() {}
-  method2() {}
-  method3() {}
-  method4() {}
-  method5() {}
-  method6() {}
-  method7() {}
-  method8() {}
-  method9() {}
-  method10() {}
-  method11() {}
-}
-
-// 중첩 함수와 조건문이 포함된 화살표 함수
-const anotherComplex = (a, b) => {
-  const inner = (c) => {
-    if (c > 10) {
-      return c * 2;
-    } else {
-      return c / 2;
-    }
-  };
-  let result = 0;
-  for (let i = 0; i < a; i++) {
-    if (i % 3 === 0) {
-      result += inner(i);
-    } else {
-      result += i;
+    if (a < 0) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (i === j) {
+            result += i * j;
+          } else {
+            result -= i + j;
+          }
+        }
+      }
     }
   }
   return result;
-};
+}
+
+// 메서드가 많은 클래스를 통한 과도한 클래스 의존성 예제
+class BigClass {
+  method1() { return 1; }
+  method2() { return 2; }
+  method3() { return 3; }
+  method4() { return 4; }
+  method5() { return 5; }
+  method6() { return 6; }
+  method7() { return 7; }
+  method8() { return 8; }
+  method9() { return 9; }
+  method10() { return 10; }
+  method11() { return 11; }
+  method12() { return 12; }
+  method13() { return 13; }
+  method14() { return 14; }
+}
+
+// 중복 코드가 있는 함수 예제
+function duplicateCodeFunction(a) {
+  // 중복되는 계산 로직
+  let x = a + 10;
+  let y = a * 2;
+  let z = x + y;
+
+  // 동일한 로직이 다시 반복됨 (중복 코드)
+  let x2 = a + 10;
+  let y2 = a * 2;
+  let z2 = x2 + y2;
+
+  return z + z2;
+}
+
+// 함수 호출 (실행을 위한 간단한 예)
+console.log("Complex Function Result:", complexFunction(1, 2, 3));
+const bc = new BigClass();
+console.log("BigClass method1:", bc.method1());
+console.log("Duplicate Code Function Result:", duplicateCodeFunction(5));
