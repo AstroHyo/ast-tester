c2a90b96b1f8c7b2f49a9044f296e97e86aca154
Ethan (Seung Hyo) Lee <52388644+AstroHyo@users.noreply.github.com>
2025-05-05 01:34:01 -0400

    Create todo_app.js

---
+++ todo_app.js
@@ -0,0 +1,39 @@
+function todo_manager(list, TYPE) {
+  var maxItems = 10;
+  
+  let todoList = [];
+  let completed_items = [];
+  
+  if (list.length > maxItems) {
+    console.log("Error: Too many items");
+    return null;
+  }
+  
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].done == true) {
+      completed_items.push(list[i]);
+    } else {
+      if (TYPE == 1) {
+        list[i].priority = "high";
+      } else if (TYPE == 2) {
+        list[i].priority = "medium";
+      } else {
+        list[i].priority = "low";
+      }
+      todoList.push(list[i]);
+    }
+  }
+  
+  for (var j = 0; j < todoList.length; j++) {
+    console.log("Todo: " + todoList[j].text);
+  }
+  
+  for (var k = 0; k < completed_items.length; k++) {
+    console.log("Completed: " + completed_items[k].text);
+  }
+  
+  return {
+    todos: todoList,
+    completed: completed_items
+  };
+}
